f99eb195f08558e2dd3eead9e70744deb98ba988
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-05 11:10:39 -0700

    added basic css file and linked to from base template

--- src/main/java/net/jeremiahshore/treehouse/Main.java
+++ src/main/java/net/jeremiahshore/treehouse/Main.java
@@ -10,12 +10,14 @@ import java.util.Map;
 
 import static  spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
 /**
  * Created by Jeremiah Shore at 3/4/2018 10:13 PM for course-ideas-rebuild.
  **/
 public class Main {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
         get("/hello", (request, response) -> "Hello World!");
