4f77d27cc9e438aa64f5f56764352b6c885574fb
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-04 23:35:59 -0700

    updated signin route and welcome template to use cookies

--- src/main/java/net/jeremiahshore/treehouse/Main.java
+++ src/main/java/net/jeremiahshore/treehouse/Main.java
@@ -3,17 +3,31 @@ package net.jeremiahshore.treehouse;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static  spark.Spark.get;
+import static spark.Spark.post;
 
 /**
  * Created by Jeremiah Shore at 3/4/2018 10:13 PM for course-ideas-rebuild.
  **/
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World!");
+        get("/hello", (request, response) -> "Hello World!");
 
-        get("/", (req, res) ->
-                new ModelAndView(null, "index.hbs"),
-                new HandlebarsTemplateEngine());
+        get("/", (request, response) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", request.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+            }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (request, response) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = request.queryParams("username");
+            response.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
+            }, new HandlebarsTemplateEngine());
     }
 }
