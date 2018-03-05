47c3b6937d7293fb117272b7859bb48a62e91394
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-04 22:20:42 -0700

    added first basic route

--- src/main/java/net/jeremiahshore/treehouse/Main.java
+++ src/main/java/net/jeremiahshore/treehouse/Main.java
@@ -1,10 +1,12 @@
 package net.jeremiahshore.treehouse;
 
+import static  spark.Spark.get;
+
 /**
  * Created by Jeremiah Shore at 3/4/2018 10:13 PM for course-ideas-rebuild.
  **/
 public class Main {
     public static void main(String[] args) {
-
+        get("/hello", (req, res) -> "Hello World!");
     }
 }
