e6e975a69b5883a4f49c66e251ee659b59e48234
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-04 22:15:22 -0700

    Initial commit

---
+++ src/main/java/net/jeremiahshore/treehouse/Main.java
@@ -0,0 +1,10 @@
+package net.jeremiahshore.treehouse;
+
+/**
+ * Created by Jeremiah Shore at 3/4/2018 10:13 PM for course-ideas-rebuild.
+ **/
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
