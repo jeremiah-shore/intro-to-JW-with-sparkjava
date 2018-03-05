54f7538850e7cc43e8e6de0f6728c2aae6d460ee
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-04 22:25:34 -0700

    added second basic route for root

--- src/main/java/net/jeremiahshore/treehouse/Main.java
+++ src/main/java/net/jeremiahshore/treehouse/Main.java
@@ -8,5 +8,7 @@ import static  spark.Spark.get;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World!");
+
+        get("/", (req, res) -> "Welcome students!");
     }
 }
