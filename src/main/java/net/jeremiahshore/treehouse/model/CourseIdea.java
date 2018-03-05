628f9f8e42a15776d76bd970cbb761ea93d24aa8
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-05 12:08:02 -0700

    abstracted routes away from cookie imlpementation,added filter to add username attribute which routes can use instead

--- src/main/java/net/jeremiahshore/treehouse/model/CourseIdea.java
+++ src/main/java/net/jeremiahshore/treehouse/model/CourseIdea.java
@@ -1,15 +1,30 @@
 package net.jeremiahshore.treehouse.model;
 
+import com.github.slugify.Slugify;
+
+import java.io.IOException;
+
 /**
  * Created by Jeremiah Shore at 3/5/2018 10:36 AM for course-ideas-rebuild.
  **/
 public class CourseIdea {
+    private String slug;
     private String title;
     private String creator;
 
     public CourseIdea(String title, String creator) {
         this.title = title;
         this.creator = creator;
+        try {
+            Slugify slugify = new Slugify();
+            this.slug = slugify.slugify(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSlug() {
+        return slug;
     }
 
     public String getTitle() {
---
+++ src/main/java/net/jeremiahshore/treehouse/model/NotFoundException.java
@@ -0,0 +1,7 @@
+package net.jeremiahshore.treehouse.model;
+
+/**
+ * Created by Jeremiah Shore at 3/5/2018 12:05 PM for course-ideas-rebuild.
+ **/
+public class NotFoundException extends RuntimeException{
+}
