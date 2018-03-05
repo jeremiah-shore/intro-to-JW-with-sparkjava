23090a81289085dc85ac938479b1944ab697f32d
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-04 22:39:33 -0700

    added route directing /hello to recent template

--- src/main/java/net/jeremiahshore/treehouse/Main.java
+++ src/main/java/net/jeremiahshore/treehouse/Main.java
@@ -1,5 +1,8 @@
 package net.jeremiahshore.treehouse;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static  spark.Spark.get;
 
 /**
@@ -9,6 +12,8 @@ public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World!");
 
-        get("/", (req, res) -> "Welcome students!");
+        get("/", (req, res) ->
+                new ModelAndView(null, "index.hbs"),
+                new HandlebarsTemplateEngine());
     }
 }
